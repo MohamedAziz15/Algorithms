54ab3dad053860078e576529a05bc4aa74cf9a78
MohamedAziz15 <101571364+MohamedAziz15@users.noreply.github.com>
2022-12-24 00:52:30 +0200

    first version

---
+++ insertion sort Aziz/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace InsertionSort
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InsertionSortBTN = new MetroFramework.Controls.MetroButton();
+            this.beforeSortedLBL = new MetroFramework.Controls.MetroLabel();
+            this.metroTextBox1 = new MetroFramework.Controls.MetroTextBox();
+            this.afterSortedLBL = new MetroFramework.Controls.MetroLabel();
+            this.SuspendLayout();
+            // 
+            // InsertionSortBTN
+            // 
+            this.InsertionSortBTN.Location = new System.Drawing.Point(392, 149);
+            this.InsertionSortBTN.Name = "InsertionSortBTN";
+            this.InsertionSortBTN.Size = new System.Drawing.Size(93, 32);
+            this.InsertionSortBTN.TabIndex = 0;
+            this.InsertionSortBTN.Text = "insertion";
+            this.InsertionSortBTN.UseSelectable = true;
+            this.InsertionSortBTN.Click += new System.EventHandler(this.SortBTN_Click);
+            // 
+            // beforeSortedLBL
+            // 
+            this.beforeSortedLBL.AutoSize = true;
+            this.beforeSortedLBL.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.beforeSortedLBL.FontWeight = MetroFramework.MetroLabelWeight.Bold;
+            this.beforeSortedLBL.Location = new System.Drawing.Point(103, 233);
+            this.beforeSortedLBL.Name = "beforeSortedLBL";
+            this.beforeSortedLBL.Size = new System.Drawing.Size(56, 25);
+            this.beforeSortedLBL.TabIndex = 2;
+            this.beforeSortedLBL.Text = "array";
+            this.beforeSortedLBL.UseStyleColors = true;
+            // 
+            // metroTextBox1
+            // 
+            // 
+            // 
+            // 
+            this.metroTextBox1.CustomButton.Image = null;
+            this.metroTextBox1.CustomButton.Location = new System.Drawing.Point(248, 2);
+            this.metroTextBox1.CustomButton.Name = "";
+            this.metroTextBox1.CustomButton.Size = new System.Drawing.Size(27, 27);
+            this.metroTextBox1.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroTextBox1.CustomButton.TabIndex = 1;
+            this.metroTextBox1.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroTextBox1.CustomButton.UseSelectable = true;
+            this.metroTextBox1.CustomButton.Visible = false;
+            this.metroTextBox1.Lines = new string[0];
+            this.metroTextBox1.Location = new System.Drawing.Point(94, 149);
+            this.metroTextBox1.MaxLength = 32767;
+            this.metroTextBox1.Multiline = true;
+            this.metroTextBox1.Name = "metroTextBox1";
+            this.metroTextBox1.PasswordChar = '\0';
+            this.metroTextBox1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.metroTextBox1.SelectedText = "";
+            this.metroTextBox1.SelectionLength = 0;
+            this.metroTextBox1.SelectionStart = 0;
+            this.metroTextBox1.ShortcutsEnabled = true;
+            this.metroTextBox1.Size = new System.Drawing.Size(278, 32);
+            this.metroTextBox1.TabIndex = 3;
+            this.metroTextBox1.UseSelectable = true;
+            this.metroTextBox1.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.metroTextBox1.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            this.metroTextBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.metroTextBox1_KeyPress);
+            // 
+            // afterSortedLBL
+            // 
+            this.afterSortedLBL.AutoSize = true;
+            this.afterSortedLBL.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.afterSortedLBL.FontWeight = MetroFramework.MetroLabelWeight.Bold;
+            this.afterSortedLBL.ForeColor = System.Drawing.Color.Crimson;
+            this.afterSortedLBL.Location = new System.Drawing.Point(103, 306);
+            this.afterSortedLBL.Name = "afterSortedLBL";
+            this.afterSortedLBL.Size = new System.Drawing.Size(56, 25);
+            this.afterSortedLBL.TabIndex = 4;
+            this.afterSortedLBL.Text = "array";
+            this.afterSortedLBL.UseCustomForeColor = true;
+            this.afterSortedLBL.UseStyleColors = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.afterSortedLBL);
+            this.Controls.Add(this.metroTextBox1);
+            this.Controls.Add(this.beforeSortedLBL);
+            this.Controls.Add(this.InsertionSortBTN);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroButton InsertionSortBTN;
+        private MetroFramework.Controls.MetroLabel beforeSortedLBL;
+        private MetroFramework.Controls.MetroTextBox metroTextBox1;
+        private MetroFramework.Controls.MetroLabel afterSortedLBL;
+    }
+}
+
---
+++ insertion sort Aziz/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace InsertionSort
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void SortBTN_Click(object sender, EventArgs e)
+        {
+            //int[] myInts = Array.ConvertAll(metroTextBox1.Text.ToArray, int.Parse);
+            //string ss =metroTextBox1.Text;
+            //string[] arr1 = new[] { ss };
+            //int[] arr = new int[] {int.Parse(ss)};
+            //List<int> hh = arr1.ToList<int>;
+            //(metroTextBox1.Text).ToArray;
+            // int[] arrayInt = Array.ConvertAll(arr1, s => int.Parse(s));
+
+            //int[] arr = new int[Convert.ToInt32(arr1)];
+            // int[] arr = metroTextBox1.Text.ToList;
+            //int[] sorted = insertionSort((int[])metroTextBox1.Text.ToArray, lenght);
+            //try
+            //{
+            //    Image1.Width = Convert.ToInt32(metroTextBox1.Text);
+            //}
+            //catch (System.FormatException)
+            //{
+            //    Image1.Width = 100; // or other default value as appropriate in context.
+            //}
+            int [] array = { 1 * Convert.ToInt32(metroTextBox1.Text) };
+            int length = array.Length;
+            int[]sorted = insertionSort(array, length);
+            beforeSortedLBL.Text = sorted.ToString();
+
+        }
+        List<double> beforeListSotring = new List<double>();
+        double[] list;
+        private int[] insertionSort(int[] array, int lenght)
+        {
+           // array = { Convert.ToInt32(insertArrayTB.Text) };
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                var key = array[i];
+                var flag = 0;
+
+                for (int j = i - 1; j >= 0 && flag != 1;)
+                {
+                    if (key < array[j])
+                    {
+                        array[j + 1] = array[j];
+                        j--;
+                        array[j + 1] = key;
+                    }
+                    else flag = 1;
+                }
+            }
+
+            return array;
+        }
+
+        private void metroTextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+                list.Append<double>(double.Parse(metroTextBox1.Text));
+                //beforeListSotring.Add(Convert.ToDouble(metroTextBox1.Text));
+                beforeSortedLBL.Text = beforeListSotring.ToString();
+                beforeSortedLBL.Text = list.ToString();
+            }
+        }
+    }
+}
